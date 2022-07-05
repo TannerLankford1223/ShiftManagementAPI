4892cb202e0c3069337eda44f1d526802dfef48a
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-05 02:24:10 -0500

    Initialize shift service

--- shift-service/src/main/java/com/example/shiftservice/ShiftServiceApplication.java
+++ shift-service/src/main/java/com/example/shiftservice/ShiftServiceApplication.java
@@ -1,9 +1,17 @@
 package com.example.shiftservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Shift Service API", version = "1.0", description = "Documentation for the Shift Service API v1.0"))
 public class ShiftServiceApplication {
 
     public static void main(String[] args) {
