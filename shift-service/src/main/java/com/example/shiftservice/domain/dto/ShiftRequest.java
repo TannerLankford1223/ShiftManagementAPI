40df84ddb4def895e7d1b34eb877f2919ffd8666
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-07 13:29:13 -0500

    Add Lombok builder annotation

--- shift-service/src/main/java/com/example/shiftservice/domain/dto/ShiftRequest.java
+++ shift-service/src/main/java/com/example/shiftservice/domain/dto/ShiftRequest.java
@@ -2,6 +2,7 @@ package com.example.shiftservice.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.time.LocalTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ShiftRequest{
 
     @JsonProperty("employee_id")
--- shift-service/src/main/java/com/example/shiftservice/domain/dto/ShiftResponse.java
+++ shift-service/src/main/java/com/example/shiftservice/domain/dto/ShiftResponse.java
@@ -2,17 +2,18 @@ package com.example.shiftservice.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ShiftResponse {
 
     @JsonProperty("shift_id")
@@ -29,7 +30,6 @@ public class ShiftResponse {
     private String employeeEmail;
 
     @JsonProperty("date")
-    @Pattern(regexp = "[0-9]{2}/[0-9]{2}/[0-9]{4}")
     private LocalDate shiftDate;
 
     @JsonProperty("start_time")
