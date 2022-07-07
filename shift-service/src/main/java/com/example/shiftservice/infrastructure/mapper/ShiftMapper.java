9c96b3d61dc808a1f1ae39434bf249019c99fb9a
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-07 13:30:29 -0500

    Refactor: make mapper an abstract class

--- shift-service/src/main/java/com/example/shiftservice/infrastructure/mapper/ShiftMapper.java
+++ shift-service/src/main/java/com/example/shiftservice/infrastructure/mapper/ShiftMapper.java
@@ -1,14 +1,33 @@
 package com.example.shiftservice.infrastructure.mapper;
 
+import com.example.shiftservice.domain.dto.Employee;
 import com.example.shiftservice.domain.dto.ShiftRequest;
 import com.example.shiftservice.domain.dto.ShiftResponse;
 import com.example.shiftservice.infrastructure.entity.Shift;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
-public interface ShiftMapper {
+@Mapper(componentModel = "spring")
+abstract class ShiftMapper {
+    public ShiftResponse shiftToShiftResponse(Shift shift, Employee employee) {
+        return ShiftResponse.builder()
+                .id(shift.getId())
+                .firstName(employee.getFirstName())
+                .lastName(employee.getLastName())
+                .employeeEmail(employee.getEmail())
+                .shiftDate(shift.getShiftDate())
+                .startTime(shift.getStartTime())
+                .endTime(shift.getEndTime())
+                .build();
+    }
 
-    ShiftResponse shiftToShiftResponse(Shift shift);
-
-    Shift shiftRequestToShift(ShiftRequest shiftRequest);
+    @Mapping(target = "shift.id", ignore = true)
+    public Shift shiftRequestToShift(ShiftRequest request) {
+        return Shift.builder()
+                .employeeId(request.getEmployeeId())
+                .shiftDate(request.getShiftDate())
+                .startTime(request.getStartTime())
+                .endTime(request.getEndTime())
+                .build();
+    }
 }
