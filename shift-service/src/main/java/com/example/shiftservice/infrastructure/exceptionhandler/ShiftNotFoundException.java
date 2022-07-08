0038e858027dc66126a8873410c0f2dce3b1ac21
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-07 20:09:09 -0500

    Refactor: change exception to accept shift id

--- shift-service/src/main/java/com/example/shiftservice/infrastructure/exceptionhandler/ShiftNotFoundException.java
+++ shift-service/src/main/java/com/example/shiftservice/infrastructure/exceptionhandler/ShiftNotFoundException.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.Serial;
-import java.time.LocalDate;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ShiftNotFoundException extends RuntimeException {
@@ -12,7 +11,7 @@ public class ShiftNotFoundException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public ShiftNotFoundException(String email, LocalDate date) {
-        super("Shift for employee with email " + email + " and date " + date + " not found");
+    public ShiftNotFoundException(long shiftId) {
+        super("Shift with id " + shiftId + " not found");
     }
 }
