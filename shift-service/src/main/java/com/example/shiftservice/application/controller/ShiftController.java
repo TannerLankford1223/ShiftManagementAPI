27122e769a15390c7ddce64ce56d5701d803fca4
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-07 23:18:08 -0500

    Implement methods

--- shift-service/src/main/java/com/example/shiftservice/application/controller/ShiftController.java
+++ shift-service/src/main/java/com/example/shiftservice/application/controller/ShiftController.java
@@ -5,6 +5,7 @@ import com.example.shiftservice.domain.dto.ScheduleRequest;
 import com.example.shiftservice.domain.dto.ShiftRequest;
 import com.example.shiftservice.domain.dto.ShiftResponse;
 import com.example.shiftservice.domain.ports.api.ShiftServicePort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,29 +23,31 @@ public class ShiftController {
 
     @PostMapping("/new-shift")
     public ShiftResponse createShift(@RequestBody ShiftRequest shift) {
-        return null;
+        return shiftService.createShift(shift);
     }
 
-    @GetMapping("/{email}")
-    public ShiftResponse getEmployeeShift(@PathVariable String email) {
-        return null;
+    @GetMapping("/{shiftId}")
+    public ShiftResponse getEmployeeShift(@PathVariable long shiftId) {
+        return shiftService.getEmployeeShift(shiftId);
     }
 
     // Returns list of shifts for an employee for a given time period if the employee ID is specified in the
     // request body. Otherwise, it returns shifts for all employees.
     @GetMapping("/schedule")
     public List<DailySchedule> getWorkSchedule(@RequestBody ScheduleRequest scheduleRequest) {
-        return null;
+        return shiftService.getWorkSchedule(scheduleRequest);
     }
 
     // Posts a list of shifts for a given time period
     @PostMapping("/schedule")
     public ResponseEntity<String> postWorkSchedule(@RequestBody List<ShiftRequest> shiftRequests) {
-        return null;
+        shiftService.postWorkSchedule(shiftRequests);
+        return new ResponseEntity<>("Work schedule successfully posted", HttpStatus.OK);
     }
 
     @DeleteMapping("/{shiftId}")
     public ResponseEntity<String> deleteEmployeeShift(@PathVariable long shiftId) {
-        return null;
+        shiftService.deleteEmployeeShift(shiftId);
+        return new ResponseEntity<>("Shift successfully deleted", HttpStatus.OK);
     }
 }
