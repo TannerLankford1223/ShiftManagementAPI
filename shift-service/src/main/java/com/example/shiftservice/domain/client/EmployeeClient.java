0eb05af42d816f3cec4059afd49577b5924ff202
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-07 20:04:23 -0500

    Refactor: change client method

--- shift-service/src/main/java/com/example/shiftservice/domain/client/EmployeeClient.java
+++ shift-service/src/main/java/com/example/shiftservice/domain/client/EmployeeClient.java
@@ -1,21 +1,24 @@
 package com.example.shiftservice.domain.client;
 
-import com.example.shiftservice.domain.dto.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 @FeignClient(name = "employee-service", url = "http://localhost:8282", fallback = EmployeeFallback.class)
 public interface EmployeeClient {
 
-    @GetMapping("/api/employees/{employeeId}")
-    Employee getEmployee(@PathVariable long employeeId);
+    @GetMapping("/api/employees/exists/{employeeId}")
+    boolean employeeExists(@PathVariable long employeeId);
 }
 
+@Slf4j
 class EmployeeFallback implements EmployeeClient {
 
     @Override
-    public Employee getEmployee(long employeeId) {
-        return null;
+    public boolean employeeExists(long employeeId) {
+        log.error("Unable to connect to employee service server");
+        return false;
     }
 }
