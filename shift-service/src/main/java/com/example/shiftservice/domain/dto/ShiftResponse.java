cb0c5aefda199c7709edf5deded50b42283b2327
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-07 23:14:25 -0500

    Refactor: convert time fields to 12 hr format

--- shift-service/src/main/java/com/example/shiftservice/domain/dto/ShiftResponse.java
+++ shift-service/src/main/java/com/example/shiftservice/domain/dto/ShiftResponse.java
@@ -1,19 +1,18 @@
 package com.example.shiftservice.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class ShiftResponse {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
 
     @JsonProperty("shift_id")
     private long id;
@@ -25,8 +24,16 @@ public class ShiftResponse {
     private LocalDate shiftDate;
 
     @JsonProperty("start_time")
-    private LocalTime startTime;
+    private String startTime;
 
     @JsonProperty("end_time")
-    private LocalTime endTime;
+    private String endTime;
+
+    public ShiftResponse(long id, long employeeId, LocalDate shiftDate, LocalTime startTime, LocalTime endTime) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.shiftDate = shiftDate;
+        this.startTime = formatter.format(startTime);
+        this.endTime = formatter.format(endTime);
+    }
 }
