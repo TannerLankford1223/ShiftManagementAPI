d13885f161ccc38338c9253303ca4bded8685f1f
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-08 23:23:29 -0500

    Add necessary annotations

--- employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -1,9 +1,17 @@
 package com.example.employeeservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Employee Service API", version = "1.0", description = "Documentation for the Employee Service API v1.0"))
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
