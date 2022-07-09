85e7c5ae1f22b528754ff5d78b59e5ddb2ee8c9b
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-08 23:22:22 -0500

    Create Jpa adapter for the employee repository

---
+++ employee-service/src/main/java/com/example/employeeservice/infrastructure/adapters/EmployeeJpaAdapter.java
@@ -0,0 +1,36 @@
+package com.example.employeeservice.infrastructure.adapters;
+
+import com.example.employeeservice.domain.ports.spi.EmployeePersistencePort;
+import com.example.employeeservice.infrastructure.entity.Employee;
+import com.example.employeeservice.infrastructure.persistence.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeJpaAdapter implements EmployeePersistencePort {
+
+    private final EmployeeRepository employeeRepo;
+
+    public EmployeeJpaAdapter(EmployeeRepository employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployee(long employeeId) {
+        return null;
+    }
+
+    @Override
+    public Employee deleteEmployee(long employeeId) {
+        return null;
+    }
+
+    @Override
+    public boolean employeeExists(long employeeId) {
+        return false;
+    }
+}
