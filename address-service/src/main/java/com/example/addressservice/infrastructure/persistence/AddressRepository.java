80c4979a41560659d89bd7ba932add4425f832cc
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 22:34:53 -0500

    Create Address JPA repository

---
+++ address-service/src/main/java/com/example/addressservice/infrastructure/persistence/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.example.addressservice.infrastructure.persistence;
+
+import com.example.addressservice.infrastructure.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, String> {
+}
