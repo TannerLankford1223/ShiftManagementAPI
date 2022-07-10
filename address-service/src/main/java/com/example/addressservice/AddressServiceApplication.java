3c0584475e1dbc722736abc366c85900c783ca3d
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 22:24:50 -0500

    Add necessary annotations

--- address-service/src/main/java/com/example/addressservice/AddressServiceApplication.java
+++ address-service/src/main/java/com/example/addressservice/AddressServiceApplication.java
@@ -1,8 +1,14 @@
 package com.example.addressservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@OpenAPIDefinition(info =
+@Info(title = "Address Service API", version = "1.0", description = "Documentation for the Address Service API v1.0"))
 @SpringBootApplication
 public class AddressServiceApplication {
 
