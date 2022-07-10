dbbfdbcfed5c0bc290e06d7a52dfb91cc6177da5
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 23:11:31 -0500

    Implement persistence port methods

---
+++ address-service/src/main/java/com/example/addressservice/infrastructure/adapters/AddressJpaAdapter.java
@@ -0,0 +1,44 @@
+package com.example.addressservice.infrastructure.adapters;
+
+import com.example.addressservice.domain.ports.spi.AddressPersistencePort;
+import com.example.addressservice.infrastructure.entity.Address;
+import com.example.addressservice.infrastructure.persistence.AddressRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class AddressJpaAdapter implements AddressPersistencePort {
+
+    private final AddressRepository addressRepo;
+
+    public AddressJpaAdapter(AddressRepository addressRepo) {
+        this.addressRepo = addressRepo;
+    }
+
+    @Override
+    public Address saveAddress(Address address) {
+        return addressRepo.save(address);
+    }
+
+    @Override
+    public Optional<Address> getAddress(String storeId) {
+        return addressRepo.findById(storeId);
+    }
+
+    @Override
+    public List<Address> getAddresses() {
+        return addressRepo.findAll();
+    }
+
+    @Override
+    public List<Address> getAddressesInState(String state) {
+        return addressRepo.findAllByState(state);
+    }
+
+    @Override
+    public Address deleteAddress(String storeId) {
+        return addressRepo.deleteByStoreId(storeId);
+    }
+}
