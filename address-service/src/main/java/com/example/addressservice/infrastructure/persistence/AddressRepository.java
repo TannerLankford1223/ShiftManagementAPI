deb2eb0a656247bf174918d71d5eb69e88277dd2
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 23:09:43 -0500

    Add repository method to delete address

--- address-service/src/main/java/com/example/addressservice/infrastructure/persistence/AddressRepository.java
+++ address-service/src/main/java/com/example/addressservice/infrastructure/persistence/AddressRepository.java
@@ -4,6 +4,12 @@ import com.example.addressservice.infrastructure.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
+
+    List<Address> findAllByState(String state);
+
+    Address deleteByStoreId(String storeId);
 }
