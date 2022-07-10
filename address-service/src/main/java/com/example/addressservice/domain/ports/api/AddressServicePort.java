3979f99681583681dfb089bcbe525c119ed18d2b
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 22:51:18 -0500

    Create API and SPI ports

---
+++ address-service/src/main/java/com/example/addressservice/domain/ports/api/AddressServicePort.java
@@ -0,0 +1,20 @@
+package com.example.addressservice.domain.ports.api;
+
+import com.example.addressservice.domain.dto.AddressDTO;
+
+import java.util.List;
+
+public interface AddressServicePort {
+
+    AddressDTO saveAddress(AddressDTO addressDTO);
+
+    AddressDTO getAddress(String storeId);
+
+    List<AddressDTO> getAddresses();
+
+    List<AddressDTO> getAddressesInState(String state);
+
+    AddressDTO updateAddress(AddressDTO addressDTO);
+
+    void deleteAddress(String storeId);
+}
---
+++ address-service/src/main/java/com/example/addressservice/domain/ports/spi/AddressPersistencePort.java
@@ -0,0 +1,19 @@
+package com.example.addressservice.domain.ports.spi;
+
+import com.example.addressservice.infrastructure.entity.Address;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressPersistencePort {
+
+    Address saveAddress(Address address);
+
+    Optional<Address> getAddress(String storeId);
+
+    List<Address> getAddresses();
+
+    List<Address> getAddressesInState(String state);
+
+    void deleteAddress(String storeId);
+}
