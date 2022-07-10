ab74503fd0486bc3a53c2ed6eee0426228cb5650
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 22:34:28 -0500

    Create Address entity

---
+++ address-service/src/main/java/com/example/addressservice/infrastructure/entity/Address.java
@@ -0,0 +1,26 @@
+package com.example.addressservice.infrastructure.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "address")
+public class Address {
+
+    @Id
+    private String storeId;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private int zip;
+}
