41f3055fd791e5b26288693750f349f52d5b854e
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-09 22:07:17 -0500

    Refactor: remove unnecessary Feign dependencies

--- employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -5,9 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 @OpenAPIDefinition(info =
