51fba0aa41d50aa2fb73db65ea54f91511cba05a
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-10 02:21:39 -0500

    Implement methods for web layer

--- address-service/src/main/java/com/example/addressservice/application/controller/AddressController.java
+++ address-service/src/main/java/com/example/addressservice/application/controller/AddressController.java
@@ -1,42 +1,52 @@
 package com.example.addressservice.application.controller;
 
 import com.example.addressservice.domain.dto.AddressDTO;
+import com.example.addressservice.domain.ports.api.AddressServicePort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/address")
+@RequestMapping("/api/address")
 public class AddressController {
 
+    private final AddressServicePort addressService;
+
+    public AddressController(AddressServicePort addressService) {
+        this.addressService = addressService;
+    }
+
     @PostMapping("/new-address")
-    public AddressDTO saveAddress(AddressDTO addressDTO) {
-        return null;
+    public AddressDTO saveAddress(@RequestBody AddressDTO addressDTO) {
+        return addressService.saveAddress(addressDTO);
     }
 
     @GetMapping("/{storeId}")
     public AddressDTO getAddress(@PathVariable String storeId) {
-        return null;
+        return addressService.getAddress(storeId);
     }
 
-    @GetMapping("")
+    @GetMapping("/address-list")
     public List<AddressDTO> getAddressList() {
-        return null;
+        return addressService.getAddresses();
     }
 
-    @GetMapping("/{state}")
+    @GetMapping("/address-list/{state}")
     public List<AddressDTO> getAddressListByState(@PathVariable String state) {
-        return null;
+        return addressService.getAddressesInState(state);
     }
 
     @PutMapping("/update/{storeId}")
-    public AddressDTO updateAddress(@PathVariable String storeId, AddressDTO addressDTO) {
-        return null;
+    public AddressDTO updateAddress(@PathVariable String storeId, @RequestBody AddressDTO addressDTO) {
+        addressDTO.setStoreId(storeId);
+        return addressService.updateAddress(addressDTO);
     }
 
     @DeleteMapping("/{storeId}")
     public ResponseEntity<String> deleteAddress(@PathVariable String storeId) {
-        return null;
+        addressService.deleteAddress(storeId);
+        return new ResponseEntity<>("Store with id " + storeId + " deleted", HttpStatus.OK);
     }
 }
