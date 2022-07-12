b92f76c6563215829362954fe6d02ad01b330e44
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-12 12:00:53 -0500

    Implement routes to services

---
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayConfig.java
@@ -0,0 +1,36 @@
+package com.example.gatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder routeBuilder) {
+
+        return routeBuilder.routes()
+                .route(r -> r.path("/api/address/**")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("fallback")
+                                .setFallbackUri("forward:/fallback/address")))
+                        .uri("lb://address-service"))
+
+                .route(r -> r.path("/api/employee/**")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("fallback")
+                                .setFallbackUri("forward:/fallback/employee")))
+                        .uri("lb://employee-service"))
+
+                .route(r -> r.path("/api/shift/**")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("fallback")
+                                .setFallbackUri("forward:/fallback/shift")))
+                        .uri("lb://shift-service"))
+
+                .route(r -> r.path("/api/email/**")
+                        .filters(f -> f.circuitBreaker(config -> config.setName("fallback")
+                                .setFallbackUri("forward:/fallback/email")))
+                        .uri("lb://email-service"))
+                .build();
+    }
+}
