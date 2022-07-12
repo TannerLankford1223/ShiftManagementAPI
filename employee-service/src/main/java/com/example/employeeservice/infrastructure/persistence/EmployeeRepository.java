af7c73c259917c107521cae0dc0be77cbb3ea9fe
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-12 12:20:50 -0500

    Refactor: change return type to void

--- employee-service/src/main/java/com/example/employeeservice/infrastructure/persistence/EmployeeRepository.java
+++ employee-service/src/main/java/com/example/employeeservice/infrastructure/persistence/EmployeeRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    Employee deleteById(long employeeId);
+    void deleteById(long employeeId);
 }
