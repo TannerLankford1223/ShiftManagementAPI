d0c6bf745930abefcb40156822d8eeccb81323b0
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-13 17:19:20 -0500

    Refactor: use regex for zip code validation

--- address-service/src/main/java/com/example/addressservice/domain/dto/AddressDTO.java
+++ address-service/src/main/java/com/example/addressservice/domain/dto/AddressDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
@@ -32,7 +32,7 @@ public class AddressDTO {
 
     @JsonProperty("zip_code")
     @NotNull
-    @Size(min = 5, max = 5, message = "Zip Code must be 5 digits in length")
+    @Pattern(regexp = "^\\d{5}(?:[-\\s]\\d{4})?$")
     private String zipCode;
 
     public AddressDTO(String address, String city, String state, String zipCode) {
