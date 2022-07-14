9a0703bf02cdd37a0efa6ef0f208fbae8c5d05c1
TannerLankford1223 <T.Lankford730@gmail.com>
2022-07-13 22:22:33 -0500

    Add controller for handling validation errors

---
+++ employee-service/src/main/java/com/example/employeeservice/infrastructure/exceptionhandler/ErrorModel.java
@@ -0,0 +1,25 @@
+package com.example.employeeservice.infrastructure.exceptionhandler;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorModel {
+
+    private final HttpStatus status;
+
+    private final LocalDateTime timestamp;
+
+    private final String message;
+
+    private final String details;
+
+    public ErrorModel(HttpStatus status, String message, String details) {
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+        this.message = message;
+        this.details = details;
+    }
+}
