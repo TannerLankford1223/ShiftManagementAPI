78a5434c6c4c8bb8f1b8695539ec7ac6fdee6e38
TannerLankford1223 <T.Lankford730@gmail.com>
2022-08-05 01:04:37 -0500

    Remove dependencies for Oauth2

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,11 +2,10 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
+@SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
--- keycloak.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --restart unless-stopped -d -p 9900:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name keycloak jboss/keycloak:12.0.3
